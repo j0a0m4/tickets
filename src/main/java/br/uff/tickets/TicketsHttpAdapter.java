cc92b677198af13f4d217cf883c2166240176cd8
João Lopes <joaomarcoslopes@id.uff.br>
2022-10-13 21:25:06 -0300

    feat: get hello world

---
+++ src/main/java/br/uff/tickets/TicketsHttpAdapter.java
@@ -0,0 +1,16 @@
+package br.uff.tickets;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/tickets")
+public class TicketsHttpAdapter {
+
+    @GetMapping
+    public ResponseEntity<String> getHelloWorld() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
